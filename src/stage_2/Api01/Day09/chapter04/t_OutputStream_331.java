1aa5cb50c8f0ab9cf14a41c4b1d70bfffb1872a4
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-04-26 01:39:26 +0800

    JavaLearn

--- src/stage_2/Api01/Day09/chapter04/t_OutputStream_331.java
+++ src/stage_2/Api01/Day09/chapter04/t_OutputStream_331.java
@@ -31,6 +31,7 @@ package Api01.Day09.chapter04;
             1、创建FileOutputStream对象，构造方法中传递写入数据的目的地
             2、调用FileOutputStream对象中的write方法，把数据写入到文件中
             3、释放资源（流使用会占用一定的内存，使用完毕需把内存释放，提高程序的效率）
+
  */
 
 import java.io.FileOutputStream;
