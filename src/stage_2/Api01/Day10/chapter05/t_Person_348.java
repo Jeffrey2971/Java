4dd3dbb2002821373e7d0c58f76c2fdfb0fe084b
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-04-28 00:11:48 +0800

    JavaLearn

--- src/stage_2/Api01/Day10/chapter05/t_Person_348.java
+++ src/stage_2/Api01/Day10/chapter05/t_Person_348.java
@@ -1,6 +1,18 @@
 package Api01.Day10.chapter05;
 
-public class t_Person_348 {
+/*
+    序列化和反序列化的时候，会抛出NotSerializableException没有序列化异常
+    通过java.io.Serializable接口以启用序列化功能，未实现此接口的类无法使用其他任何状态的序列化或反序列化
+    Serializable接口也叫标记型接口
+        要进行序列化和反序列化的类必须实现Serializable接口，就会给这个类添加一个标记，当进行序列化和反序列化的时候，就会检测类上是否有这个标记
+            有：可序列化和反序列化
+            没有：NotSerializableException
+    去市场卖肉-->肉上都有一个蓝色的章（检测合格）-->放心使用-->买回来怎么吃随意
+ */
+
+import java.io.Serializable;
+
+public class t_Person_348 implements Serializable {
     private String name;
     private int age;
 
