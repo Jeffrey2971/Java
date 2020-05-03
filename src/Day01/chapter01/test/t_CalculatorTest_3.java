85e7f79be4a69833fadfbcb90c3073974073e76e
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-03 22:19:58 +0800

    JavaLearn

--- src/Day01/chapter01/test/t_CalculatorTest_3.java
+++ src/Day01/chapter01/test/t_CalculatorTest_3.java
@@ -1,16 +1,41 @@
 package Day01.chapter01.test;
 
 import Day01.chapter01.Junit.t_Calculator_1;
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class t_CalculatorTest_3 {
+    /**
+     * 初始化方法
+     *  用于资源的申请，所有测试方法在执行之前都会先执行该方法
+     *  用法：@Before
+     */
+    @Before
+    public void init(){
+        System.out.println("init... ...");
+    }
+
+    /**
+     * 释放资源的方法：
+     *  在所有测试方法执行完毕之后都会自动执行该方法
+     *  用法：@After
+     */
+    @After
+    public void close(){
+        System.out.println("close... ...");
+
+    }
+
+
     /**
      * 测试add方法
      */
     @Test
     public void TestAdd() {
         // System.out.println("我被执行了");
+        System.out.println("testAdd... ...");
         // 1、创建计算器对象
         t_Calculator_1 c = new t_Calculator_1();
         // 2、调用add方法
@@ -25,7 +50,9 @@ public class t_CalculatorTest_3 {
     @Test
     public void testSub(){
         t_Calculator_1 c = new t_Calculator_1();
+
         int result = c.sub(1, 2);
+        System.out.println("testSub");
         Assert.assertEquals(-1, result);
     }
 }
