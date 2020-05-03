0d4c14ef7ff380ccf5927db25dec118ade79f1d3
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-03 17:12:46 +0800

    JavaLearn

--- src/Day01/chapter01/test/t_CalculatorTest_3.java
+++ src/Day01/chapter01/test/t_CalculatorTest_3.java
@@ -1,6 +1,7 @@
 package Day01.chapter01.test;
 
 import Day01.chapter01.Junit.t_Calculator_1;
+import org.junit.Assert;
 import org.junit.Test;
 
 public class t_CalculatorTest_3 {
@@ -8,13 +9,16 @@ public class t_CalculatorTest_3 {
      * 测试add方法
      */
     @Test
-    public void TestAdd(){
+    public void TestAdd() {
         // System.out.println("我被执行了");
         // 1、创建计算器对象
         t_Calculator_1 c = new t_Calculator_1();
-        int add = c.add(1, 2);
-        System.out.println(add);
         // 2、调用add方法
+        int result = c.add(1, 2);
+        // System.out.println(add);
+
+        // 3、断言     我断言这个结果是三
+        Assert.assertEquals(3, result);
 
     }
 }
