cc6d389f6f73086cead720eef6daad3ebf211adb
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-03 16:12:25 +0800

    JavaLearn

---
+++ src/Day01/chapter01/t_CalculatorTest_2.java
@@ -0,0 +1,11 @@
+package Day01.chapter01;
+
+public class t_CalculatorTest_2 {
+    public static void main(String[] args) {
+        // 创建对象
+        t_Calculator_1 c = new t_Calculator_1();
+        // 调用方法
+        int result = c.add(2, 3);
+        System.out.println(result);
+    }
+}
---
+++ src/Day01/chapter01/t_Calculator_1.java
@@ -0,0 +1,28 @@
+package Day01.chapter01;
+
+/*
+    计算器类
+ */
+
+public class t_Calculator_1 {
+    /**
+     * 加法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int add(int a ,int b){
+        return a+b;
+    }
+
+    /**
+     * 减法
+     * @param a
+     * @param b
+     * @return
+     */
+    public int sub(int a ,int b){
+        return a-b;
+    }
+
+}
