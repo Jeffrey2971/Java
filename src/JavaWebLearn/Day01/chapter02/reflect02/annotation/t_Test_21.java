e2c7a0a5b6f84253d453d5d2707f9d44b4cf361e
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-04 21:59:35 +0800

    JavaLearn

---
+++ src/JavaWebLearn/Day01/chapter02/reflect02/annotation/t_Test_21.java
@@ -0,0 +1,7 @@
+package JavaWebLearn.Day01.chapter02.reflect02.annotation;
+
+public class t_Test_21 {
+    public void show(){
+        System.out.println("show1...");
+    }
+}
---
+++ src/JavaWebLearn/Day01/chapter02/reflect02/annotation/t_Test_22.java
@@ -0,0 +1,7 @@
+package JavaWebLearn.Day01.chapter02.reflect02.annotation;
+
+public class t_Test_22 {
+    public void show(){
+        System.out.println("show2...");
+    }
+}
