8de5f40d49cec7f7b92340bff41512ac5941d40b
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-10 00:41:06 +0800

    JavaLearn

--- MyCode/JavaLearn/stage_4/JDBC/jdbc/jdbcTest11.java
+++ MyCode/JavaLearn/stage_4/JDBC/jdbc/jdbcTest11.java
@@ -5,7 +5,6 @@ import JDBC.util.jdbcUtils;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
-import java.sql.SQLException;
 
 /**
  * 实务操作
@@ -18,6 +17,8 @@ public class jdbcTest11 {
         try {
             // 1、获取连接
             conn = jdbcUtils.getConnection();
+            // 开启事务
+            conn.setAutoCommit(false);
             // 2、定义SQL
             String sql_1 = "update account set balance = balance - ? where id = ?";
             String sql_2 = "update account set balance = balance + ? where id = ?";
@@ -26,19 +27,31 @@ public class jdbcTest11 {
             pstmt2 = conn.prepareStatement(sql_2);
 
             //4、设置参数
-            pstmt1.setDouble(1,500);
-            pstmt1.setDouble(2,1);
-            pstmt2.setDouble(1,500);
-            pstmt2.setDouble(2,2);
+            pstmt1.setDouble(1, 500);
+            pstmt1.setDouble(2, 1);
+            pstmt2.setDouble(1, 500);
+            pstmt2.setDouble(2, 2);
 
             // 5、执行SQL语句
             pstmt1.executeUpdate();
+            // 手动制造异常
+            int i = 3 / 0;
             pstmt2.executeUpdate();
-        } catch (SQLException throwables) {
+            // 提交事务
+            conn.commit();
+        } catch (Exception throwables) {
+            // 只要出现异常都会执行这里的代码块，所以把回滚事务放到catch里
             throwables.printStackTrace();
-        }finally {
-            jdbcUtils.close(pstmt1,conn);
-            jdbcUtils.close(pstmt2,null);
+            try {
+                if (conn != null) {
+                    conn.rollback();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } finally {
+            jdbcUtils.close(pstmt1, conn);
+            jdbcUtils.close(pstmt2, null);
         }
 
     }
