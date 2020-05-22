a06e9a1e26977e9d80ef9c709f387c495d6a955a
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-22 22:54:07 +0800

    JavaWebLearn

--- MyCode/JavaLearn/cookie2/src/cookie/CookieDemo.java
+++ MyCode/JavaLearn/cookie2/src/cookie/CookieDemo.java
@@ -8,21 +8,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/*
-Cookie快速入门
- */
-
-@WebServlet("/cookieDemo5")
-public class CookieDemo5 extends HttpServlet {
+@WebServlet("/CookieDemo")
+public class CookieDemo extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // 创建Cookie对象
-        Cookie c1 = new Cookie("msg", "你好呀");
-        // 设置path，让当前服务器下部署的项目，共享cookie信息
-        c1.setPath("/");
-        // 发送cookie
-        response.addCookie(c1);
+// 获取cookie信息
+        // 获取cookie
+        System.out.println("cookie2");
+        Cookie[] cookies = request.getCookies();
+        // 获取数据，遍历cookie
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                String name = cookie.getName();
+                String value = cookie.getValue();
 
-
+                System.out.println(name + "：" + value);
+            }
+        }
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
