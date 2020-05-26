0592ed3d4019b2d90ae70b860209feae49665a93
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-27 01:18:24 +0800

    JavaWebLearn

---
+++ MyCode/JavaLearn/Filter&Listener/src/web/filter/FilterDemo1.java
@@ -0,0 +1,28 @@
+package web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+/**
+ * 过滤器
+ */
+// @WebFilter("/*") // 访问所有资源之前都会执行该顾虑器
+public class FilterDemo1 implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        System.out.println("FilterDemo1被执行了...");
+        // 放行
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ MyCode/JavaLearn/Filter&Listener/src/web/filter/FilterDemo2.java
@@ -0,0 +1,28 @@
+package web.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter("/*")
+public class FilterDemo2 implements Filter {
+    public void destroy() {
+    }
+
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        // 对request对象的请求消息增强
+        System.out.println("filterDemo2Do...");
+        /*
+        放行
+         */
+
+        chain.doFilter(req, resp);
+        // 对response对象的响应消息增强
+        System.out.println("filterDemoBack...");
+    }
+
+    public void init(FilterConfig config) throws ServletException {
+
+    }
+
+}
