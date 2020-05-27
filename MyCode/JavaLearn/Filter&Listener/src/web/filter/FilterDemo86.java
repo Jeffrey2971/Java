08298c1bd4889007bbca8457b05e75499398980e
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-27 19:01:36 +0800

    JavaWebLearn

--- MyCode/JavaLearn/Filter&Listener/src/web/filter/FilterDemo86.java
+++ MyCode/JavaLearn/Filter&Listener/src/web/filter/FilterDemo86.java
@@ -5,14 +5,14 @@ import javax.servlet.annotation.WebFilter;
 import java.io.IOException;
 
 @WebFilter("/*")
-public class FilterDemo6 implements Filter {
+public class FilterDemo86 implements Filter {
     public void destroy() {
     }
 
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        System.out.println("Filter6执行了");
+        System.out.println("Filter86执行了");
         chain.doFilter(req, resp);
-        System.out.println("Filter6回来了");
+        System.out.println("Filter86回来了");
     }
 
     public void init(FilterConfig config) throws ServletException {
