5c154228356814f1ed5baeef4ec642690f19e80f
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-28 02:31:34 +0800

    JavaWebLearn

--- MyCode/JavaLearn/Filter&Listener/src/proxy/proxyTest.java
+++ MyCode/JavaLearn/Filter&Listener/src/proxy/proxyTest.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Proxy;
 public class proxyTest {
     public static void main(String[] args) {
         // 创建真实对象
-        Lenovo lenovo = new Lenovo();
+        final Lenovo lenovo = new Lenovo();
 
 
         // 动态代理增强Lenovo对象
@@ -20,15 +20,42 @@ public class proxyTest {
          */
 
         saleComputer proxy_lenovo = (saleComputer) Proxy.newProxyInstance(lenovo.getClass().getClassLoader(), lenovo.getClass().getInterfaces(), new InvocationHandler() {
+            /*
+                代理逻辑编写的方法：所有代理对象调用的所有方法都会触发该方法执行
+                    - 参数
+                        - proxy：代理对象
+                        - method：代理对象调用的方法被封装为对象
+                        - args：代理对象调用方法时传递的实际参数
+
+             */
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                return null;
+                /*System.out.println("该方法执行了");
+                System.out.println(method.getName());
+                System.out.println(args[0]);*/
+
+                // 增强参数
+                if (method.getName().equals("sale")) {
+                    double money = (double) args[0];
+                    money = money * 0.85;
+                    System.out.println("专车接送");
+                    // 使用真实对象调用该方法
+                    Object obj = method.invoke(lenovo, money);
+                    System.out.println("免费送货");
+                    return obj + "_鼠标";
+                }else{
+                    Object obj = method.invoke(lenovo, args);
+                    return obj;
+                }
+
+
             }
         });
 
 
         // 调用方法
-        String computer = lenovo.sale(9000);
+        String computer = proxy_lenovo.sale(9000);
         System.out.println(computer);
+//        proxy_lenovo.show();
     }
 }
--- MyCode/JavaLearn/SelectUserCase/src/web/filter/SensitiveWordsFilter.java
+++ MyCode/JavaLearn/SelectUserCase/src/web/filter/SensitiveWordsFilter.java
@@ -3,6 +3,7 @@ package web.filter;
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.lang.reflect.InvocationHandler;
@@ -12,7 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * 铭感词汇过滤器
+ * 敏感词汇过滤
  */
 
 @WebFilter("/*")
@@ -20,55 +21,49 @@ public class SensitiveWordsFilter implements Filter {
     public void destroy() {
     }
 
-    public void doFilter(final ServletRequest req, ServletResponse resp, FilterChain chain) throws ServletException, IOException {
-        // 1、创建代理对象，增强getParameter方法
-
+    public void doFilter(final ServletRequest req, final ServletResponse resp, FilterChain chain) throws ServletException, IOException {
+        // 创建代理对象，增强Parameter方法
         ServletRequest proxy_req = (ServletRequest) Proxy.newProxyInstance(req.getClass().getClassLoader(), req.getClass().getInterfaces(), new InvocationHandler() {
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                // 增强getParameter方法
-                // 判断是否是getParameter方法
                 if(method.getName().equals("getParameter")){
                     // 增强返回值
-                    // 获取返回值
                     String value = (String) method.invoke(req, args);
                     if(value!=null){
                         for (String str : list) {
                             if(value.contains(str)){
                                 value = value.replaceAll(str, "***");
                             }
-
                         }
                     }
-
                     return value;
                 }
+
                 return method.invoke(req, args);
             }
         });
-
-        // 2、放行
-                chain.doFilter(proxy_req, resp);
+        // 放行
+        chain.doFilter(proxy_req, resp);
     }
 
-    private List<String> list = new ArrayList<String>(); // 铭感词汇集合
+    private List<String> list = new ArrayList<String>(); // 敏感词汇
     public void init(FilterConfig config) throws ServletException {
         try {
-            // 获取文件真实路径
-            ServletContext servletContext = config.getServletContext();
-            String realPath = servletContext.getRealPath("/WEB-INF/classes/敏感词汇.txt");
-            // 读取文件
+            // 获取文件的真实
+            String realPath = config.getServletContext().getRealPath("/WEB-INF/classes/敏感词汇.txt");
             BufferedReader br = new BufferedReader(new FileReader(realPath));
-            // 将文件的每一行添加到list集合中
+            // 读取文件
             String line = null;
             while ((line = br.readLine()) != null){
                 list.add(line);
             }
             br.close();
-            System.out.println(list);
-        } catch (Exception e) {
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
             e.printStackTrace();
         }
+        // 将文件每一行的数据添加到list中
     }
 
 }
