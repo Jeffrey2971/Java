0c862242957eaa0248fd33240ea1790e514ac345
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-05-28 00:00:32 +0800

    JavaWebLearn

--- MyCode/JavaLearn/Filter&Listener/src/proxy/proxyTest.java
+++ MyCode/JavaLearn/Filter&Listener/src/proxy/proxyTest.java
@@ -7,7 +7,7 @@ import java.lang.reflect.Proxy;
 public class proxyTest {
     public static void main(String[] args) {
         // 创建真实对象
-        Lenovo lenovo = new Lenovo();
+        final Lenovo lenovo = new Lenovo();
 
         // 动态代理增强Lenovo对象
 
@@ -20,11 +20,35 @@ public class proxyTest {
         SaleComputer proxy_lenovo = (SaleComputer) Proxy.newProxyInstance(lenovo.getClass().getClassLoader(), lenovo.getClass().getInterfaces(), new InvocationHandler() {
             /*
                 代理逻辑编写的方法：代理对象调用的所有方法都会出发该方法执行
+                    - 参数
+                        - proxy：代理对象
+                        - method：代理对象调用的方法，北封装为的对象
+                        - args：方法执行时，传递的实际参数
              */
             @Override
             public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-                System.out.println("该方法执行");
-                return null;
+                /*System.out.println("该方法执行");
+                System.out.println(method.getName());
+                System.out.println(args[0]);*/
+
+
+                // 判断是否是sale方法
+                if (method.getName().equals("sale")) {
+                    // 增强参数
+                    double money = (double) args[0];
+                    money = money * 0.85;
+                    System.out.println("专车接送");
+                    // 使用真实对象调用该方法
+                    String obj = (String) method.invoke(lenovo, money);
+                    System.out.println("免费送货");
+                    // 增强返回值
+
+                    return obj + "_鼠标垫";
+                }else{
+                    Object obj = method.invoke(lenovo, args);
+                    return obj;
+                }
+
             }
         });
 
@@ -32,5 +56,6 @@ public class proxyTest {
 
         String computer = proxy_lenovo.sale(8000);
         System.out.println(computer);
+//        proxy_lenovo.show();
     }
 }
