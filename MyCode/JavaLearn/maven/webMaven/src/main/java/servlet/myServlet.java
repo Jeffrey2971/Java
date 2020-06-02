23567080c2b197ebf4ecd4b9f8dc2f7a1ff78421
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-06-02 20:18:03 +0800

    JavaLearn

--- MyCode/JavaLearn/maven/webMaven/src/main/java/servlet/myServlet.java
+++ MyCode/JavaLearn/maven/webMaven/src/main/java/servlet/myServlet.java
@@ -6,10 +6,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-
-public class Servlet extends HttpServlet {
+public class myServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/hello.jsp").forward(request,response);
+        request.getRequestDispatcher("/hello.jsp").forward(request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- MyCode/JavaLearn/maven/webMaven/src/main/webapp/hello.jsp
+++ MyCode/JavaLearn/maven/webMaven/src/main/webapp/hello.jsp
@@ -2,7 +2,7 @@
   Created by IntelliJ IDEA.
   User: jeffrey
   Date: 2020/6/2
-  Time: 上午1:14
+  Time: 下午3:48
   To change this template use File | Settings | File Templates.
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -11,6 +11,9 @@
     <title>Title</title>
 </head>
 <body>
-
+<%
+    System.out.println("HelloWorld");
+    return;
+%>
 </body>
 </html>
