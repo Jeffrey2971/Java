d93c3a8da5d4c6ee3ca1535b13644ec9b9f44bb3
Jeffrey2971 <Jeffrey2971@outlook.com>
2020-06-05 02:44:45 +0800

    JavaLearn

--- MyCode/JavaLearn/maven/travel/src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
+++ MyCode/JavaLearn/maven/travel/src/main/java/cn/itcast/travel/web/servlet/RouteServlet.java
@@ -1,11 +1,12 @@
 package cn.itcast.travel.web.servlet;
 
+import cn.itcast.travel.domain.PageBean;
+import cn.itcast.travel.domain.Route;
 import cn.itcast.travel.service.RouteService;
 import cn.itcast.travel.service.impl.RouteServiceImpl;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -32,7 +33,7 @@ public class RouteServlet extends BaseServlet {
 
         int cid = 0; // 类别id
         int currentPage = 0; // 当前的页码，如果不传递则默认为第一页
-        int PageSizeStr = 0; // 每页显示条数，如果不传递，默认每页显示5条记录
+        int PageSize = 0; // 每页显示条数，如果不传递，默认每页显示5条记录
         // 处理参数
         if(cidStr != null && cidStr.length() >0){ // 判断字符串是否为空和是否为空字符串
             cid = Integer.parseInt(cidStr); // 传递回来的是字符串，应该转换为int类型
@@ -46,14 +47,16 @@ public class RouteServlet extends BaseServlet {
 
 
         if(pageSizeStr != null && pageSizeStr.length() >0){ // 判断字符串是否为空和是否为空字符串
-            PageSizeStr = Integer.parseInt(pageSizeStr); // 传递回来的是字符串，应该转换为int类型
+            PageSize = Integer.parseInt(pageSizeStr); // 传递回来的是字符串，应该转换为int类型
         }else{
-            PageSizeStr = 5;
+            PageSize = 5;
         }
 
         // 调用Service查询PageBean对象
+        PageBean<Route> pb = routeService.pageQuery(cid, currentPage, PageSize);
 
         // 将PageBean对象序列化为json
+        writeValue(pb, response);
 
 
 
